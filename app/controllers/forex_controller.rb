d804cdd6002778444da0fa602d16be1995073726
s-b-edu <sbaur@chicagobooth.edu>
2021-07-20 12:52:47 +0000

    done with assignment

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -10,4 +10,12 @@ class ForexController < ApplicationController
     @symbol_data = JSON.parse(open(symbol_url).read).dig("symbols")
     render({ :template => "forex_templates/first_currency.html.erb" })
   end
+  def conversion
+    @from_currency = params.fetch("first_currency")
+    @to_currency = params.fetch("second_currency")
+    conversion_url = "https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}"
+    conversion_data = JSON.parse(open(conversion_url).read)
+    @rate = conversion_data.dig("info","rate")
+    render({ :template => "forex_templates/conversion.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/forex", { :controller => "forex", :action => "setup" })
   get("/forex/:first_currency", { :controller => "forex", :action => "first_currency" })
+  get("/forex/:first_currency/:second_currency", { :controller => "forex", :action => "conversion" })
 end
