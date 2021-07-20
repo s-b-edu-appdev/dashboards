5fc2a25b9ccb578b7022aae492e597747d17b623
s-b-edu <sbaur@chicagobooth.edu>
2021-07-20 12:44:19 +0000

    done with building out currency list items

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -1,5 +1,13 @@
 class ForexController < ApplicationController
   def setup
+    symbol_url = "https://api.exchangerate.host/symbols"
+    @symbol_data = JSON.parse(open(symbol_url).read).dig("symbols")
     render({ :template => "forex_templates/main_page.html.erb" })
+  end
+  def first_currency
+    @from_currency = params.fetch("first_currency")
+    symbol_url = "https://api.exchangerate.host/symbols"
+    @symbol_data = JSON.parse(open(symbol_url).read).dig("symbols")
+    render({ :template => "forex_templates/first_currency.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get("/forex", { :controller => "forex", :action => "setup" })
+  get("/forex/:first_currency", { :controller => "forex", :action => "first_currency" })
 end
